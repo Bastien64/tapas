a239601514a0ec09cc3f257884bb8f11008e4577
Bastien VILLEGAS <95182085+Bastien64@users.noreply.github.com>
2023-09-18 19:10:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+window.onload = function() {
+    setTimeout(function() {
+      document.getElementById("nav-section").style.opacity = 1;
+    }, 2000);
+  };
+  
+  // Sélectionnez la section2
+const section2 = document.querySelector('.section2');
+
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+
+            section2.classList.add('in-view');
+        } else {
+
+            section2.classList.remove('in-view');
+        }
+    });
+});
+observer.observe(section2);
+
+
+
+const section3 = document.querySelector('.section3');
+const observer3 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+
+            section3.classList.add('in-view');
+        } else {
+            section3.classList.remove('in-view');
+        }
+    });
+});
+observer3.observe(section3);
+
+
+
+// Sélectionnez la section4
+const section4 = document.querySelector('.section4');
+
+const observer4 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+
+            section4.classList.add('in-view');
+        } else {
+            section4.classList.remove('in-view');
+        }
+    });
+});
+
+// Observez la section4
+observer4.observe(section4);
